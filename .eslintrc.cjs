01c5b0c9f2034779c1e368bb8e3b7b085ec4fea2
Richard Beaver <beaver.r@northeastern.edu>
2023-10-07 11:58:52 -0400

    Apply Prettier formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,20 +2,20 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
+    "eslint:recommended",
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
-    'plugin:react-hooks/recommended',
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
-  parser: '@typescript-eslint/parser',
+  ignorePatterns: ["dist", ".eslintrc.cjs", "vite.config.ts"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
   },
-  plugins: ['react-refresh', '@typescript-eslint'],
+  plugins: ["react-refresh", "@typescript-eslint"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
     // These opinionated rules are enabled in stylistic-type-checked above.
@@ -31,4 +31,4 @@ module.exports = {
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
   },
-}
+};
