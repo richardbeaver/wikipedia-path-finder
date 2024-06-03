4ab71c6b44538efcf183175c051ac041a8efd14d
Richard Beaver <beaver.r@northeastern.edu>
2024-06-03 10:38:58 -0400

    Change bfs strategy
    Instead of using the `parents` dict as a way to mark a title
    as visited (and only doing so after finding all the linked titles in its
    html), use a separate `seen` hashset that includes all titles that we
    have found that have been linked to by some other page. We now
    collect all titles that have been seen, not just ones that we've
    searched the html of already.
    
    Also rename the `to_visit` deque to `queue` to remove its affect on how
    we think about what pages we've "visited" so far.

--- api/wikipedia_crawler.py
+++ api/wikipedia_crawler.py
@@ -1,5 +1,4 @@
-from collections import deque, defaultdict
-import functools
+from collections import deque
 import requests
 from bs4 import BeautifulSoup
 
@@ -16,30 +15,37 @@ class WikipediaCrawler:
 
     def __init__(self, starting_page_title: str):
         self.start: str = starting_page_title
-        self.to_visit: deque[str] = deque([starting_page_title])
-        self.parents: defaultdict[str, set[str]] = defaultdict(set)
+        self.seen: set[str] = set([starting_page_title])
+        self.queue: deque[str] = deque([starting_page_title])
+        self.parents: dict[str, str] = {}
 
     def crawl(self) -> list[str] | None:
         if self.start == KEVIN_BACON_TITLE:
             return [KEVIN_BACON_TITLE]
 
-        pages = 0
-        while len(self.to_visit) != 0:
-            cur_title = self.to_visit.popleft()
+        visited_pages = 0
+
+        while len(self.queue) != 0:
+            print(f"visited {visited_pages} pages")
+
+            cur_title = self.queue.popleft()
             linked_titles = self._get_linked_titles(cur_title)
-            pages += 1
-            print(f"done {pages} pages")
 
             for linked_title in linked_titles:
-                if linked_title in self.parents:
+                if linked_title in self.seen:
                     continue
 
-                self.parents[linked_title].add(cur_title)
+                self.parents[linked_title] = cur_title
 
                 if linked_title == KEVIN_BACON_TITLE:
-                    return self._get_path()
+                    full_path = self._get_path()
+                    if full_path is None:
+                        return full_path
+                    return list(full_path)
 
-                self.to_visit.append(linked_title)
+                self.queue.append(linked_title)
+                self.seen.add(linked_title)
+                visited_pages += 1
 
         return None
 
@@ -60,34 +66,69 @@ class WikipediaCrawler:
 
         return linked_titles
 
-    # OPTIMIZATION: add a `maxsize` to the cache
-    @functools.lru_cache
+    # Single parent version
     def _get_path(
         self,
-        starting_path: tuple[str] | None = None,
-    ) -> list[str] | None:
-        if starting_path is None:
-            starting_path = (KEVIN_BACON_TITLE,)
+    ) -> deque[str] | None:
+        path = deque([KEVIN_BACON_TITLE])
 
-        cur_title = starting_path[0]
+        while path[0] != self.start:
+            parent = self.parents[path[0]]
+            path.appendleft(parent)
 
-        if cur_title == self.start:
-            return list(starting_path)
+        return path
 
-        parents = self.parents[cur_title]
+    # "set of parents" version
+    # def _get_path_rec(
+    #     self,
+    #     # next_parent: str,
+    #     starting_path: deque[str] | None = None,
+    # ) -> deque[str] | None:
+    #     # if len(starting_path) == 0:
+    #     #     if next_parent is not KEVIN_BACON_TITLE:
+    #     #         raise ValueError(
+    #     #             "First title must be kevin bacon title if starting path is empty"
+    #     #         )
 
-        if len(parents) == 0:
-            return None
+    #     assert starting_path is None or starting_path[-1] == KEVIN_BACON_TITLE
 
-        resulting_paths: list[list[str]] = []
+    #     if starting_path is None:
+    #         starting_path = deque([KEVIN_BACON_TITLE])
 
-        for parent in parents:
-            new_path = self._get_path((parent,) + starting_path)
-            if new_path is not None:
-                resulting_paths.append(new_path)
+    #     cur_title = starting_path[0]
 
-        if len(resulting_paths) == 0:
-            return None
+    #     # if next_parent == self.start:
+    #     #     starting_path.appendleft(next_parent)
+    #     #     return starting_path
 
-        resulting_paths.sort(key=len)
-        return resulting_paths[0]
+    #     parents = self.parents[cur_title]
+    #     if len(parents) == 0:
+    #         return None
+
+    #     # shortest_path: deque[str] | None = None
+    #     shortest_path_parent: str | None = None
+
+    #     # starting_path.appendleft(next_parent)
+    #     for parent in parents:
+    #         starting_path.appendleft(parent)
+    #         new_path = self._get_path(starting_path)
+    #         starting_path.popleft(parent)
+
+    #         if new_path is None:
+    #             continue
+
+    #         # if shortest_path is None or len(new_path) < len(shortest_path):
+    #         #     shortest_path = new_path
+
+    #     if shortest_path is None:
+    #         return None
+
+    #     return shortest_path
+
+
+if __name__ == "__main__":
+    FOOTLOOSE_TITLE = "Footloose_(1984_film)"
+    HERBERT_ROSS_TITLE = "Herbert_Ross"
+
+    crawler = WikipediaCrawler(HERBERT_ROSS_TITLE)
+    assert crawler.crawl() == [HERBERT_ROSS_TITLE, FOOTLOOSE_TITLE, KEVIN_BACON_TITLE]
